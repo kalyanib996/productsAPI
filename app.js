6a440a2cb7b4a0436b2b64457aa6a19201ae25fc
kalyanib996 <kalyanib996@gmail.com>
2024-06-20 20:06:46 -0600

    enabling cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require('cors');
 require("dotenv").config();
 const app = express();
 const PORT  = process.env.PORT || 5000;
 const dbConnection = require("./db/connect");
 const products_routes = require("./routes/products");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Liveee");
 });
