689ccf1762bf55f369097c4489cad0a421eebf13
kalyanib996 <kalyanib996@gmail.com>
2024-06-20 19:30:29 -0600

    changing Port for railway

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
-const PORT  = process.env.port || 5000;
+const PORT  = process.env.PORT || 5000;
 const dbConnection = require("./db/connect");
 const products_routes = require("./routes/products");
 app.get("/", (req, res) => {
