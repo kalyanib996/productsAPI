b9868642dbd755da49ed109e92974a9c8c23eacf
kalyanib996 <kalyanib996@gmail.com>
2024-06-21 23:09:07 -0600

    adding query to filter and select specific product by id

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,23 @@
+const Product = require("../models/product");
 
-const Product= require("../models/product");
-const getAllProducts=async(req,res)=>{
-const prodData= await Product.find({})
-res.status(200).json({prodData})
+const getAllProducts = async (req, res) => {
+  
+
+  if (req.query) {
+    console.log(req.query)
+    const prodData = await Product.find(req.query);
+    res.status(200).json({ prodData });
+  }else{
+  const prodData = await Product.find({});
+  res.status(200).json({ prodData });
+}
 };
 
-const getAllProductsTesting=async(req,res)=>{
-    console.log("Here")
-    res.status(200).json({msg:"getAll Products testing"})
+
+
+const getAllProductsTesting = async (req, res) => {
+  console.log("Here");
+  res.status(200).json({ msg: "getAll Products testing" });
 };
 
-module.exports={getAllProducts,getAllProductsTesting }
-
+module.exports = { getAllProducts, getAllProductsTesting };
