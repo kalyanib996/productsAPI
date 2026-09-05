f899f54f9d7d095f1b4dcfefe19651e6b18e370b
kalyanib996 <51087148+kalyanib996@users.noreply.github.com>
2026-09-05 18:24:32 -0400

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
-console.log(uri)
 const connectDB = async () => {
   console.log("connecting db");
   await mongoose.connect(uri, {
